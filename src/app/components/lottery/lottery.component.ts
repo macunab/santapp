619a8c2e21b39475d0899ec781da58b9d9afd452
Marshall <mn.acunab@gmail.com>
2022-11-20 19:56:09 -0300

    send multiple emails

--- src/app/components/lottery/lottery.component.ts
+++ src/app/components/lottery/lottery.component.ts
@@ -71,56 +71,49 @@ export class LotteryComponent implements OnInit {
       }
       return;
     }
-
     const lottery: Lottery = this.lotteryForm.value;
-    console.log(lottery.players);
     this.sendEmails(lottery);
-    this.lotteryForm.reset();
-    this.players.clear();
-    this.addPlayer();
   }
 
-  /* recorro la lista de players, tengo una copia de la lista a la que voy a sacar 
-    cada player que asigne como objetivo y guardare cada player en una tercera lista que voy a recorrer enviando
-    el mail con el nombre del player que le toco para regalo...
-    1) lista de player.
-    2) copia de lista de player que voy a ir desapilando players a medida que salgan seleccionados de forma random.
-    3) Lista para recorrer y enviar mail compuesta de objetos { player, name } o { player, player }
-  */
   sendEmails(lottery: Lottery): void {
-    let playersTemp: Array<Player> = lottery.players;
+    const playersTemp: Array<Player> = [ ...lottery.players];
     let secretResult: Array<LotteryResult> = [];
     let index: number;
-    console.log(playersTemp);
     for(var player of lottery.players) {
       const valueArr = lottery.players.map( item => item.email);
-      let isDuplicate = valueArr.some((item, index) => {
+      let hasDuplicates = valueArr.some((item, index) => {
         return valueArr.indexOf(item) != index;
       })
-      if(isDuplicate) {
+      if(hasDuplicates) {
         Swal.fire({
-          title: 'No puede ingresar el mismo email para diferentes participantes',
+          title: 'Asegurese que los participantes cuenten con emails diferentes.',
           icon: 'warning'
         })
         return;
       }
       do {
         index = Math.floor(Math.random() * (playersTemp.length));
-        console.log(`El index generado es: ${ index }`)
       } while (playersTemp[index].email === player.email);
       const santa: LotteryResult = { santa: player, name: playersTemp[index].name }
       secretResult.push(santa);
-      
+      Email.send({
+        SecureToken: "7e8b1e24-50a7-417e-a2d5-bcf702f6a164",
+        To: santa.santa.email,
+        From: 'sorteoloco@gmail.com',
+        Subject: 'Sorteo de SecretSanta',
+        Body: `Tu Secret Santa es : ${ santa.name }`,
+      }).then(
+        console.log('SE ENVIO TEMP probar con https githubpages')
+        )
+        playersTemp.splice(index, 1);
     }
-    Email.send({
-      SecureToken: "7e8b1e24-50a7-417e-a2d5-bcf702f6a164",
-      To: 'mn.acunab@gmail.com',
-      From: 'sorteoloco@gmail.com',
-      Subject: 'Sorteo de SecretSanta',
-      Body: 'SE ENVIO EL SORTEO',
-    }).then(
-      console.log('SE ENVIO')
-      )
+    this.lotteryForm.reset();
+    this.players.clear();
+    this.addPlayer();
+    Swal.fire({
+      title: 'El sorteo se ha realizado con exito',
+      icon: 'success'
+    })
   }
 
 }
